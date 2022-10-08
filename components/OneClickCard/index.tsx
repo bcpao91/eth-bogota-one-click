bbe0b684f9727499b9456484d50b9a032aedbd95
Brandon <bcpao91@gmail.com>
2022-10-08 15:45:37 -0700

    Completes input and drop down logic

--- components/OneClickCard/index.tsx
+++ components/OneClickCard/index.tsx
@@ -1,33 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ListDropDown from './listDropDown';
 import {
   ArrowsRightLeftIcon,
   ArrowTrendingDownIcon,
+  CurrencyDollarIcon,
 } from '@heroicons/react/20/solid';
+import * as UsdcSVG from '../../public/usdc-logo.svg';
+import * as EthSVG from '../../public/eth-logo.svg';
+import * as DaiSVG from '../../public/dai-logo.svg';
+const cryptoList = [
+  {
+    name: 'USDC',
+    symbol: UsdcSVG,
+    value: 1,
+  },
+  { name: 'ETH', symbol: EthSVG, value: 1323.53 },
+];
+const cyrptoShortList = [{ name: 'DAI', symbol: DaiSVG, value: 1 }];
+
 export default function OneClickCard() {
+  const [collateralValue, setCollateralValue] = useState<number | ''>(0);
+  const [shortValue, setShortValue] = useState<number | ''>(0);
   return (
     <div className="w-fit my-6 mx-auto">
-      <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 w-fit">
+      <div className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 w-fit">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="mb-2 text-2xl font-bold pl-1 tracking-tight text-gray-900 dark:text-white">
             One Click Short
           </h5>
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 p-1">
           {"Asset you'll be using to short"}
         </p>
         <div className="flex flex-row w-fit gap-3">
           <div>
-            <ListDropDown />
-            <input className="w-60 my-2 cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm" />
+            <h2 className="font-bold pl-1">{'Collateral'}</h2>
+            <ListDropDown
+              list={cryptoList}
+              mode={'collateral'}
+              collateralValue={collateralValue}
+              setCollateralValue={setCollateralValue}
+              shortValue={shortValue}
+              setShortValue={setShortValue}
+            />
           </div>
-          <ArrowsRightLeftIcon className="w-5" />
+          <ArrowsRightLeftIcon className="w-5 h-5 my-auto cursor-pointer" />
           <div>
-            <ListDropDown />
-            <input className="w-60 my-2 cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm" />
+            <h2 className="font-bold pl-1">{'Short'}</h2>
+            <ListDropDown
+              list={cyrptoShortList}
+              mode={'short'}
+              collateralValue={collateralValue}
+              setCollateralValue={setCollateralValue}
+              shortValue={shortValue}
+              setShortValue={setShortValue}
+            />
           </div>
         </div>
-        <div className="flex">
+        <div className="flex mt-2">
           <a
             href="#"
             className="inline-flex mx-auto py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
--- components/OneClickCard/listDropDown.tsx
+++ components/OneClickCard/listDropDown.tsx
@@ -1,28 +1,121 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
+import Image from 'next/image';
+import { CurrencyDollarIcon } from '@heroicons/react/20/solid';
 
-const people = [
-  { name: 'Wade Cooper' },
-  { name: 'Arlene Mccoy' },
-  { name: 'Devon Webb' },
-  { name: 'Tom Cook' },
-  { name: 'Tanya Fox' },
-  { name: 'Hellen Schmidt' },
-];
+type CryptoCurrency = {
+  name: string;
+  symbol: typeof import('*.svg');
+  value: number;
+};
 
-export default function ListDropDown() {
-  const [selected, setSelected] = useState(people[0]);
+type ListDropDownProps = {
+  list: CryptoCurrency[];
+  mode: string;
+  collateralValue: number | '';
+  setCollateralValue: React.Dispatch<React.SetStateAction<number | ''>>;
+  shortValue: number | '';
+  setShortValue: React.Dispatch<React.SetStateAction<number | ''>>;
+};
+
+export default function ListDropDown(props: ListDropDownProps) {
+  const [selected, setSelected] = useState<CryptoCurrency>(props.list[0]);
+  const [mode, setMode] = useState(props.mode);
+
+  const handleClick = () => {
+    if (props.collateralValue == 0) {
+      props.setCollateralValue('');
+    }
+  };
+
+  const handleBlur = () => {
+    if (props.collateralValue == '') {
+      props.setCollateralValue(0);
+    }
+  };
+
+  const handleShortCollateralValue = (value: number) => {
+    if (
+      props.mode == 'short' &&
+      (props.shortValue ?? 0) > +props.collateralValue * 0.65
+    ) {
+      props.setShortValue(+props.collateralValue * 0.65);
+      console.log('set new short value to equal 0.65 of collat');
+    } else if (props.mode == 'short') {
+      props.setShortValue(value);
+      console.log('set new short value in handleshortCollat');
+    }
+
+    if (props.mode == 'collateral') {
+      props.setCollateralValue(value);
+    }
+  };
+
+  const handleShortCollateralChange = (value: number) => {
+    if (props.mode == 'collateral') {
+      props.setCollateralValue(value);
+      props.setShortValue(value * selected.value * 0.5);
+    } else {
+      props.setShortValue(value);
+    }
+  };
+
+  const setChosenList = (value: CryptoCurrency) => {
+    setSelected(value);
+    if (props.mode == 'short') {
+      props.setShortValue(0);
+    } else {
+      props.setCollateralValue(0);
+      props.setShortValue(0);
+    }
+  };
 
   return (
-    <div className="w-60">
-      <Listbox value={selected} onChange={setSelected}>
+    <div className="w-80 h-20 z-50">
+      <Listbox
+        value={selected}
+        onChange={(newValue: CryptoCurrency) => setChosenList(newValue)}
+      >
         <div className="relative mt-1">
-          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-            <span className="block truncate">{selected.name}</span>
+          <label
+            htmlFor="collateral"
+            className="absolute flex w-full h-full p-3"
+          >
+            <input
+              className="absolute right-8 top-1.5 cursor-input text-right z-50 h-12 focus:outline-none w-24 appearance-none"
+              type={'number'}
+              value={
+                mode == 'collateral' ? props.collateralValue : props.shortValue
+              }
+              onChange={(e) =>
+                handleShortCollateralChange(+e.currentTarget.value)
+              }
+              onFocus={handleClick}
+              onBlur={handleBlur}
+            />
+            <div className="absolute right-8 top-[44px] my-auto text-gray-400 z-50 text-sm">
+              $:{' '}
+              {selected.value *
+                (mode == 'collateral'
+                  ? +props.collateralValue
+                  : +props.shortValue)}
+            </div>
+          </label>
+          <Listbox.Button className="relative flex w-full h-full items-center cursor-pointer rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <div className="flex flex-row gap-1 max-w-[120px] flex-wrap mt-3">
+              <Image
+                src={selected.symbol}
+                alt={'Crypto Symbol'}
+                width={20}
+                height={20}
+              />
+              <span className="block truncate">{selected.name}</span>
+              <div className="text-gray-600">Max: {'wallet'}</div>
+            </div>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <ChevronUpDownIcon
-                className="h-5 w-5 text-gray-400"
+                className="h-5 w-5 text-gray-400 cursor-pointer"
                 aria-hidden="true"
               />
             </span>
@@ -33,35 +126,43 @@ export default function ListDropDown() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {people.map((person, personIdx) => (
-                <Listbox.Option
-                  key={personIdx}
-                  className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
-                    }`
-                  }
-                  value={person}
-                >
-                  {({ selected }) => (
-                    <>
-                      <span
-                        className={`block truncate ${
-                          selected ? 'font-medium' : 'font-normal'
-                        }`}
-                      >
-                        {person.name}
-                      </span>
-                      {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
-                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50">
+              {props.list.map((crypto, cryptoIdx) => {
+                return (
+                  <Listbox.Option
+                    key={cryptoIdx}
+                    className={({ active }) =>
+                      `relative flex cursor-default select-none py-2 pl-3 pr-4 z-50 ${
+                        active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'
+                      }`
+                    }
+                    value={crypto}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <Image
+                          src={crypto.symbol}
+                          alt={'Crypto Symbol'}
+                          width={20}
+                          height={20}
+                        />
+                        <span
+                          className={`block truncate ml-1 ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {crypto.name}
                         </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
+                        {selected ? (
+                          <span className="absolute inset-y-0 right-0 flex items-center pr-3 text-amber-600">
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                );
+              })}
             </Listbox.Options>
           </Transition>
         </div>
