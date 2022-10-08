37ae46b58716bc4bcb871f8676955bf567f34dbd
Brandon <bcpao91@gmail.com>
2022-10-07 23:05:08 -0700

    Adds wallet connection to dapp along with come minimal styling

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { ConnectKitButton } from 'connectkit';
 
 export default function Nav() {
   return (
     <nav>
-      <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
+      <div className="flex flex-col items-center justify-center gap-4 md:flex-row border-b-2">
         <Link href={'/'}>
           <div className="cursor-pointer">Outsider Trading</div>
         </Link>
+        <ConnectKitButton />
       </div>
     </nav>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WagmiConfig, createClient } from 'wagmi';
+import { ConnectKitProvider, getDefaultClient } from 'connectkit';
+
+const alchemyId = process.env.ALCHEMY_ID;
+
+const client = createClient(
+  getDefaultClient({
+    appName: 'Outsider Trading',
+    alchemyId,
+  })
+);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider
+        customTheme={{
+          '--ck-accent-color': '#00D54B',
+          '--ck-accent-text-color': '#ffffff',
+          '--ck-primary-button-background': '#00D54B',
+        }}
+      >
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
